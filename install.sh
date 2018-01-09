d6b533d5e41f2ce9e2679810421d1e1cc5d9d987
Andrii Korzh <Andrii.Korzh@gmail.com>
2018-01-09 02:36:04 +0200

    update install

--- install.sh
+++ install.sh
@@ -10,9 +10,12 @@ CURUSER=$(whoami)
 NODE_VERSION=v6.12.3
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y install git python-virtualenv python3-virtualenv curl ntp build-essential screen cmake pkg-config libboost-all-dev libevent-dev libunbound-dev libminiupnpc-dev libunwind8-dev liblzma-dev libldns-dev libexpat1-dev libgtest-dev libzmq3-dev
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install software-properties-common
+sudo DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:certbot/certbot
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install certbot git python-virtualenv python3-virtualenv curl ntp build-essential screen cmake pkg-config libboost-all-dev libevent-dev libunbound-dev libminiupnpc-dev libunwind8-dev liblzma-dev libldns-dev libexpat1-dev libgtest-dev libzmq3-dev
 cd ~
-git clone --depth 1 -b websockets https://github.com/korzhyk/xmr-node-proxy
+git clone --depth 1 -b websockets-tmp https://github.com/korzhyk/xmr-node-proxy
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
 source ~/.nvm/nvm.sh
 nvm install $NODE_VERSION
