1ab15ecba734bfb5a3932e0741f114f09188ce59
Andrii Korzh <Andrii.Korzh@gmail.com>
2018-01-11 19:39:10 +0200

    fix install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ sudo DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:certbot/certbot
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install certbot git python-virtualenv python3-virtualenv curl ntp build-essential screen cmake pkg-config libboost-all-dev libevent-dev libunbound-dev libminiupnpc-dev libunwind8-dev liblzma-dev libldns-dev libexpat1-dev libgtest-dev libzmq3-dev
 cd ~
-git clone --depth 1 -b websockets-tmp https://github.com/korzhyk/xmr-node-proxy
+git clone --depth 1 -b websockets https://github.com/korzhyk/xmr-node-proxy
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
 source ~/.nvm/nvm.sh
 nvm install $NODE_VERSION
